45452e06d911f2be8bf39dfddafa03b05e08a005
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-11 11:36:03 -0300

    Reworked select method

--- src/main/java/com/oms/webda2/DAO/UserDAO.java
+++ src/main/java/com/oms/webda2/DAO/UserDAO.java
@@ -3,10 +3,11 @@ package com.oms.webda2.DAO;
 import com.oms.webda2.model.User;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface UserDAO {
     void insert(User user) throws SQLException;
     void update(User user) throws SQLException;
     void delete(int userId) throws SQLException;
-    void select(int userId) throws SQLException;
+    List<User> select() throws SQLException;
 }
