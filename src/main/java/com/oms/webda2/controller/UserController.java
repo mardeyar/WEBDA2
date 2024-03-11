6e32f2b9c7ba0afd04a3565ea44f35322c4bffaf
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-11 11:35:51 -0300

    Added insert method

--- src/main/java/com/oms/webda2/controller/UserController.java
+++ src/main/java/com/oms/webda2/controller/UserController.java
@@ -7,9 +7,11 @@ import static com.oms.webda2.database.SQLConnection.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserController implements UserDAO {
-    private static final String INSERT = "INSERT INTO users(first_name, last_name, civic, street, city, province, postal_code, user_name, password, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String INSERT = "INSERT INTO users(first_name, last_name, address, city, province, postal_code, email, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     private static final String UPDATE = "UPDATE users SET ?=? WHERE user_id = ?";
     private static final String DELETE = "DELETE FROM users WHERE user_id = ?";
     private static final String SELECT = "SELECT * FROM users";
@@ -17,7 +19,31 @@ public class UserController implements UserDAO {
     // Methods from UserDAO
     @Override
     public void insert(User user) throws SQLException {
+        Connection connection = null;
+        PreparedStatement stmt = null;
 
+        try {
+            connection = getConnection();
+            stmt = connection.prepareStatement(INSERT);
+
+            stmt.setString(1, user.getFirstName());
+            stmt.setString(2, user.getLastName());
+            stmt.setString(3, user.getAddress());
+            stmt.setString(4, user.getCity());
+            stmt.setString(5, user.getProvince());
+            stmt.setString(6, user.getPostalCode());
+            stmt.setString(7, user.getEmail());
+            stmt.setString(8, user.getPassword());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("SQL syntax error: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Caught exception: " + e.getMessage());
+        } finally {
+            stmt.close();
+            connection.close();
+        }
     }
 
     @Override
@@ -46,7 +72,8 @@ public class UserController implements UserDAO {
     }
 
     @Override
-    public void select(int userId) throws SQLException {
-
+    public List<User> select() throws SQLException {
+        List<User> users = new ArrayList<>();
+        return users;
     }
 }
