6af6d134627ee07ae1d37f05408246a34c9b8980
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-14 17:16:49 -0300

    Added select & insert methods

--- src/main/java/com/oms/webda2/DAO/ProductReviewDAO.java
+++ src/main/java/com/oms/webda2/DAO/ProductReviewDAO.java
@@ -3,10 +3,11 @@ package com.oms.webda2.DAO;
 import com.oms.webda2.model.ProductReview;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface ProductReviewDAO {
     void insert(ProductReview productReview) throws SQLException;
     void update(ProductReview productReview) throws SQLException;
     void delete(int productReviewId) throws SQLException;
-    void select(int productReviewId) throws SQLException;
+    List<ProductReview> select(int productId) throws SQLException;
 }
